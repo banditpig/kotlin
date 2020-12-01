2231cbbbf9d08fa1f067ba663092476f28a8c367
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2020-12-01 09:00:46 +0300

    JVM_IR KT-41911 process big arity 'invoke' arguments recursively

--- compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/lower/FunctionNVarargBridgeLowering.kt
+++ compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/lower/FunctionNVarargBridgeLowering.kt
@@ -61,7 +61,10 @@ private class FunctionNVarargBridgeLowering(val context: JvmBackendContext) :
                     this@FunctionNVarargBridgeLowering.context.ir.symbols.functionN.defaultType
                 )
                 putValueArgument(0, irArray(irSymbols.array.typeWith(context.irBuiltIns.anyNType)) {
-                    (0 until expression.valueArgumentsCount).forEach { +expression.getValueArgument(it)!! }
+                    (0 until expression.valueArgumentsCount).forEach {
+                        +expression.getValueArgument(it)!!
+                            .transform(this@FunctionNVarargBridgeLowering, null)
+                    }
                 })
             }
         }
